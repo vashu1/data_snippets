76818c4268cc9a1a84e826065c00a69570186359
vashu1 <vashu1@yandex.ru>
2020-12-23 19:53:44 +1100

    english_words

---
+++ _snippets/english_words.py
@@ -0,0 +1,28 @@
+"""
+English words with 4 double letters.
+"""
+import os
+
+if not os.path.isfile('words.txt'):
+    cmd = 'wget https://github.com/dwyl/english-words/raw/master/words.txt'
+    res = os.system(cmd)
+    if res:
+        print(f'Couldn\'t run "{cmd}". Install wget and check Internet connection.')
+        exit(1)
+
+res = []
+for line in open('words.txt').readlines():
+    word = line.strip()
+    line = list(word) # split to chars
+    pairs = []
+    for i in range(len(line)-1): # letter1, letter2 in zip(line[:-1], line[1:]):
+        letter1 = line[i]
+        letter2 = line[i+1]
+        if letter1 == letter2:
+            pairs.append(i)
+    if len(pairs) > 3:
+        print(word)
+        # look for ajacent pairs
+        #pairs2 = [(el - pairs[0]) for el in pairs]
+        #if pairs2 == list(range(0,len(pairs2)*2, 2)):
+        #    print(word)
