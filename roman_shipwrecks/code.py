1885ee54e05c74b1878fb42588a2a1bc9cb73661
vashu1 <vashu1@yandex.ru>
2021-01-02 07:08:45 +1100

    fix roman_shipwrecks

--- roman_shipwrecks/code.py
+++ roman_shipwrecks/code.py
@@ -4,28 +4,12 @@ import matplotlib.pyplot as plt
 import math
 from functools import reduce
 
-os.system('wget http://oxrep.classics.ox.ac.uk/docs/StraussShipwrecks.zip')
-os.system('unzip StraussShipwrecks.zip')
+# get dataset
+if not os.path.exists('StraussShipwrecks.zip'):
+    os.system('wget http://oxrep.classics.ox.ac.uk/docs/StraussShipwrecks.zip')
+    os.system('unzip StraussShipwrecks.zip')
 
 df = pd.read_excel('StraussShipwrecks.xlsx', engine='openpyxl')
-
-df['mid_date'] = (df['Earliest date'] + df['Latest date'])/2
-df = df.dropna(subset=['mid_date'])
-df = df[-500 <= df['mid_date']]
-df = df[df['mid_date'] <= 800]
-df['century'] = df['mid_date'].apply(lambda year:math.ceil(year/100)*100)
-
-all_wrecks = df.groupby('century')['Wreck ID'].count()
-
-plt.figure()
-all_wrecks.plot()
-plt.savefig('all_wrecks.png',bbox_inches='tight')
-plt.show()
-
-# grouped by cargo
-
-def get_groups_by_column(df, col):
-    return df[df[col]].groupby('century')['Wreck ID'].count().to_frame(name=col)
 
 """
 >>> df.columns
@@ -39,15 +23,35 @@ def get_groups_by_column(df, col):
        'Ship equipment', 'Estimated tonnage', 'Amphora type']
 """
 
+# plot all shipwrecks
+df['mid_date'] = (df['Earliest date'] + df['Latest date'])/2
+df = df.dropna(subset=['mid_date'])
+df = df[-500 <= df['mid_date']]
+df = df[df['mid_date'] <= 800]
+df['century'] = df['mid_date'].apply(lambda year:math.ceil(year/100)*100)
+
+all_wrecks = df.groupby('century')['Wreck ID'].count()
+
+all_wrecks.plot()
+plt.savefig('all_wrecks.png',bbox_inches='tight')
+plt.show()
+
+# grouped by cargo
+
+def get_groups_by_column(df, col):
+    return df[df[col]].groupby('century')['Wreck ID'].count().to_frame(name=col)
+
 cargo_columns = ['Amphorae', 'Marble', 'Columns etc', 'Sarcophagi', 'Blocks',]
 
 data = [get_groups_by_column(df, c) for c in cargo_columns]
-
 table = reduce(lambda x, y: x.merge(y, how='outer', on='century'), data)
-table = table.fillna(0)
-table['non-amphorae'] = table.apply(lambda row: sum([row[c] for c in cargo_columns if c != 'Amphorae']), axis=1)
 
-plt.figure()
+df['non-amphorae'] = df.apply(lambda row: any([row[c]==True for c in cargo_columns if c != 'Amphorae']), axis=1)
+non_amphorae = get_groups_by_column(df, 'non-amphorae')
+table = table.merge(non_amphorae, how='outer', on='century')
+
+table = table.fillna(0)
+
 table.plot()
 plt.savefig('table.png',bbox_inches='tight')
 plt.show()
