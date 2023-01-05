16b2aadb4a476093dfe5bf4daba1a9dc09ff472a
vashu1 <vashu1@yandex.ru>
2023-01-05 20:42:26 +1100

    fix z3

--- _tasks/advent2015/d07.py
+++ _tasks/advent2015/d07.py
@@ -10,9 +10,10 @@ x OR y -> e
 x LSHIFT 2 -> f
 y RSHIFT 2 -> g
 NOT x -> h
-NOT y -> i"""
+NOT y -> i
+8 RSHIFT 4 -> u"""
 lines = test.split('\n')
-#lines = open('d07.txt').readlines()
+lines = open('d07.txt').readlines()
 lines = [l.strip() for l in lines]
 
 values = {}
@@ -25,28 +26,44 @@ for line in lines:
     variables[name] = BitVec(name, BITS)
     data[name] = value.split(' ')
 
+for name in data:
+    new_vals = []
+    for val in data[name]:
+        if val in set(['NOT', 'AND', 'OR', 'LSHIFT', 'RSHIFT']):
+            new_vals.append(val)
+        elif val.isnumeric():
+            new_vals.append(BitVecVal(int(val), BITS))
+        elif val in variables:
+            new_vals.append(variables[val])
+        else:
+            assert False
+    data[name] = new_vals
+
 s = Solver()
+
+# part 2
+del data['b']
+s.add(variables['b'] == BitVecVal(46065, BITS))
 
 for name, values in data.items():
     if len(values) == 1:
-        if values[0].isnumeric():
-            s.add(variables[name] == int(value))
-        else:
-            s.add(variables[name] == variables[values[0]])
-    elif values[0] == 'NOT':
-        s.add(variables[name] == ~variables[values[1]])
-    elif values[1] == 'AND':
-        s.add(variables[name] == variables[values[0]] & variables[values[2]])
-    elif values[1] == 'OR':
-        s.add(variables[name] == variables[values[0]] | variables[values[2]])
-    elif values[1] == 'LSHIFT':
-        s.add(variables[name] == LShR(variables[values[0]], variables[values[2]]))
-    elif values[1] == 'RSHIFT':
-        s.add(variables[name] == RShR(variables[values[0]], variables[values[2]]))
+        s.add(variables[name] == values[0])
+    elif len(values) == 2 and values[0] == 'NOT':
+        s.add(variables[name] == ~values[1])
+    elif values[1] == 'AND' and len(values) == 3:
+        s.add(variables[name] == values[0] & values[2])
+    elif values[1] == 'OR' and len(values) == 3:
+        s.add(variables[name] == values[0] | values[2])
+    elif values[1] == 'LSHIFT' and len(values) == 3:
+        s.add(variables[name] == values[0] << values[2])
+    elif values[1] == 'RSHIFT' and len(values) == 3:
+        s.add(variables[name] == LShR(values[0], values[2]))
     else:
-        assert False
+        assert False, f'{name} = {values}'
 
-for name in sorted(variables.keys()):
-    print(name, s(variables[s]))
+# set_option(max_args=10000000, max_lines=1000000, max_depth=10000000, max_visited=1000000)
 
-#print(s(variables['a']))
+ch = s.check()
+assert ch == sat
+
+print(s.model().eval(variables['a']))
