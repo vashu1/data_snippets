60e6837c1a695c87cc20da51582741e8db65c3ef
vashu1 <vashu1@yandex.ru>
2024-12-18 18:28:57 +1100

    d18

---
+++ _tasks/advent2024/d18.py
@@ -0,0 +1,113 @@
+from collections import defaultdict
+
+lines = '''5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0'''
+lines = lines.split('\n')
+lines = [i.strip() for i in open('inputs/d18.txt').readlines()]
+
+
+def valid(x, y):
+	if not (0 <= x <= WHMAX):
+		return False
+	if not (0 <= y <= WHMAX):
+		return False
+	return True
+
+
+def neighbours(x, y):
+	for i in [-1, 0, +1]:
+		for j in [-1, 0, +1]:
+			if (i != 0 or j != 0) and i * j == 0:
+				x_, y_ = x + i, y + j
+				if valid(x_, y_):
+					yield (x_, y_)
+
+
+WHMAX = 70
+FALLEN = 1024
+#WHMAX = 6    # comment out for example
+#FALLEN = 12  # comment out for example
+s = 0, 0
+e = WHMAX, WHMAX
+
+grid = [[True for _ in range(WHMAX + 1)] for _ in range(WHMAX + 1)]
+for i in range(FALLEN):
+	x, y = [int(i) for i in lines[i].split(',')]
+	grid[y][x] = False
+
+steps = defaultdict(set)
+step = 1
+steps[step].add(s)
+visited = set()
+visited.add(s)
+
+while True:
+	for x, y in steps[step]:
+		if (x, y) == e:
+			break
+		for x_, y_ in neighbours(x, y):
+			if (x_, y_) not in visited and grid[y_][x_]:
+				visited.add((x_, y_))
+				steps[step+1].add((x_, y_))
+	if (x, y) == e:
+		break
+	step += 1
+
+print(step-1)
+
+
+# II
+
+
+grid = [[True for _ in range(WHMAX + 1)] for _ in range(WHMAX + 1)]
+for i in range(len(lines)):
+	bx, by = [int(i) for i in lines[i].split(',')]
+	grid[by][bx] = False
+
+	steps = defaultdict(set)
+	step = 1
+	steps[step].add(s)
+	visited = set()
+	visited.add(s)
+
+	while True:
+		if not steps[step]:
+			print(f'{bx},{by}')
+			exit()
+		for x, y in steps[step]:
+			if (x, y) == e:
+				break
+			for x_, y_ in neighbours(x, y):
+				if (x_, y_) not in visited and grid[y_][x_]:
+					visited.add((x_, y_))
+					steps[step+1].add((x_, y_))
+		if (x, y) == e:
+			break
+		step += 1
+
+
+
+
